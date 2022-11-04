e18883a2f82744264f1d9833c76a4164d9bb319b
abdel1touimi <abdel1.touimi@gmail.com>
2022-11-05 00:45:19 +0100

    config fix + global density change

--- client/client.lua
+++ client/client.lua
@@ -3,7 +3,7 @@ local function changeTypeDensity(newDensity, newDensityType, newDensityTypeLabel
   TriggerServerEvent("pp2-sceneman:server:densityChanged", newDensity, newDensityTypeLabel)
 end
 
-local menu2 = MenuV:CreateMenu(false, Lang:t("sceneman.ped_density_options"), Config.menuLocation, 220, 20, 60, 'size-125', 'none', 'menuv', 'test2')
+local menu2 = MenuV:CreateMenu(false, Lang:t("sceneman.ped_density_options"), sceneManagerConfig.menuLocation, 220, 20, 60, 'size-125', 'none', 'menuv', 'test2')
 RegisterNetEvent('pp2-sceneman:client:openMenu', function()
   MenuV:OpenMenu(menu2)
 end)
@@ -14,27 +14,31 @@ end)
 
 local vehiculesDensityValue = menu2:AddRange({ icon = '🚗', label = Lang:t("sceneman.vehicule_density_option"), min = 0, max = 10, value = 0})
 vehiculesDensityValue:On("select", function(_, value)
-  changeTypeDensity(value/10, 'vehicle', 'vehicules')
+  TriggerServerEvent("pp2-sceneman:server:changeGlobalDensity", value/10, 'vehicle', 'vehicules')
 end)
 
 local parkedVehiculesDensityValue = menu2:AddRange({ icon = '🅿️', label = Lang:t("sceneman.parked_vehicule_density_option"), min = 0, max = 10, value = 0})
 parkedVehiculesDensityValue:On("select", function(_, value)
-  changeTypeDensity(value/10, 'parked', 'parked vehicules')
+  TriggerServerEvent("pp2-sceneman:server:changeGlobalDensity", value/10, 'parked', 'parked vehicules')
 end)
 
 local pedDensityValue = menu2:AddRange({ icon = '🏃', label = Lang:t("sceneman.ped_density_option"), min = 0, max = 10, value = 0})
 pedDensityValue:On("select", function(_, value)
-  changeTypeDensity(value/10, 'peds', 'peds')
+  TriggerServerEvent("pp2-sceneman:server:changeGlobalDensity", value/10, 'peds', 'peds')
 end)
 
 local scenarioPedDensityValue = menu2:AddRange({ icon = '👨‍💻', label = Lang:t("sceneman.scenario_density_option"), min = 0, max = 10, value = 0})
 scenarioPedDensityValue:On("select", function(_, value)
-  changeTypeDensity(value/10, 'scenario', 'scenario')
+  TriggerServerEvent("pp2-sceneman:server:changeGlobalDensity", value/10, 'scenario', 'scenario')
 end)
 
 local multiplierDensityValue = menu2:AddRange({ icon = '👨‍👩‍👧‍👦', label = Lang:t("sceneman.multiplier_density_option"), min = 0, max = 10, value = 0})
 multiplierDensityValue:On("select", function(_, value)
-  changeTypeDensity(value/10, 'multiplier', 'multiplier')
+  TriggerServerEvent("pp2-sceneman:server:changeGlobalDensity", value/10, 'multiplier', 'multiplier')
+end)
+
+RegisterNetEvent("p2-sceneman:client:changeDensity", function(newDensity, newDensityType, newDensityTypeLabel)
+  changeTypeDensity(newDensity, newDensityType, newDensityTypeLabel)
 end)
 
 local removeVehiculesFromScene = menu2:AddButton({
--- server/server.lua
+++ server/server.lua
@@ -3,7 +3,11 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Commands.Add('sceneman', Lang:t("sceneman.open_scene_manager"), {}, false, function(source, _)
   TriggerClientEvent('pp2-sceneman:client:openMenu', source)
-end, 'sceneman')
+end, 'admin')
+
+RegisterNetEvent('pp2-sceneman:server:changeGlobalDensity', function(newDensity, newDensityType, newDensityTypeLabel)
+  TriggerClientEvent("p2-sceneman:client:changeDensity", newDensity, newDensityType, newDensityTypeLabel)
+end)
 
 RegisterNetEvent('pp2-sceneman:server:densityChanged', function(value, type)
   local src = source
@@ -17,7 +21,7 @@ RegisterNetEvent('pp2-sceneman:server:removeAll', function(type)
     for k,v in pairs(GetAllVehicles()) do -- Do a loop of all the known server-side objects.
         local objCoord = GetEntityCoords(v) -- Get the coord of the object found
         local dist = #(coordFrom - objCoord) -- Find the distance
-        if dist < Config.maxDistance and DoesEntityExist(v) then -- A little check here to make sure stuff doesn't break
+        if dist < sceneManagerConfig.maxDistance and DoesEntityExist(v) then -- A little check here to make sure stuff doesn't break
         --  SetEntityAsMissionEntity(v, true, true)
           DeleteEntity(v) -- Delete the entity
         end
@@ -26,7 +30,7 @@ RegisterNetEvent('pp2-sceneman:server:removeAll', function(type)
     for k,v in pairs(GetAllPeds()) do -- Do a loop of all the known server-side objects.
         local objCoord = GetEntityCoords(v) -- Get the coord of the object found
         local dist = #(coordFrom - objCoord) -- Find the distance
-        if dist < Config.maxDistance and DoesEntityExist(v) then -- A little check here to make sure stuff doesn't break
+        if dist < sceneManagerConfig.maxDistance and DoesEntityExist(v) then -- A little check here to make sure stuff doesn't break
         --  SetEntityAsMissionEntity(v, true, true)
           DeleteEntity(v) -- Delete the entity
         end
